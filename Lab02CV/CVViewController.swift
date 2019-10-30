da4c07f00e10ad387ea94ca2d4640167d5db495c
Kajo1737 <kajo1737@student.ju.se>
2019-10-30 16:34:02 +0100

    Initial Commit

---
+++ Lab02CV/CVViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CVViewController.swift
+//  Lab02CV
+//
+//  Created by Johan Kantola on 2019-10-30.
+//  Copyright © 2019 Johan Kantola. All rights reserved.
+//
+
+import UIKit
+
+class CVViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
